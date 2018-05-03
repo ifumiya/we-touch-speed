06868dd835b90975f58e9737c8709b487316e85a
PanonCotta <panoncotta@gmail.com>
2018-05-03 16:45:09 +0900

    feat: ポップアップの設定が班幣されるように変更

--- content_scripts/cs.js
+++ content_scripts/cs.js
@@ -1,9 +1,15 @@
-const PLAYBACK_RATE = 1.4;
+let playbackRate = 1.0;
 
 const changePLaybackRate = () => {
+    browser.storage.local.get("playbackRate", (value) => {
+        if (value.playbackRate) {
+            playbackRate = value.playbackRate;
+        }
+    });
     const video = document.querySelector("video");
-    if (video.playbackRate < PLAYBACK_RATE) {
-        video.playbackRate = PLAYBACK_RATE;
+    if (video.playbackRate != playbackRate) {
+        console.log(`Change playback rate ${video.playbackRate} to ${playbackRate}`);
+        video.playbackRate = playbackRate;
     }
 }
 
